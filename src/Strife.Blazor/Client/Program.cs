9e61e6573ea8aebb7b162f136551ce07affcac4e
Josh Dadak <joshdadak@gmail.com>
2020-07-20 15:13:55 +0100

    Whoops, left a using statement in place there!

--- src/Strife.Blazor/Client/Program.cs
+++ src/Strife.Blazor/Client/Program.cs
@@ -1,9 +1,7 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Strife.Blazor.Client.Auth;
-using Strife.Blazor.Client.ServiceCollectionExtensions;
 using Strife.Blazor.Shared.Models;
 using System;
 using System.Net.Http;
@@ -34,10 +32,6 @@ namespace Strife.Blazor.Client
 
             builder.Services.AddOidcAuthentication(options =>
             {
-                // Configure your authentication provider options here.
-                // For more information, see https://aka.ms/blazor-standalone-auth
-                //builder.Configuration.Bind("Local", options.ProviderOptions);
-
                 options.ProviderOptions.Authority = builder.Configuration.GetValue<string>("Auth0:Domain");
                 options.ProviderOptions.ClientId = builder.Configuration.GetValue<string>("Auth0:ClientId");
                 options.ProviderOptions.ResponseType = "token id_token";
@@ -58,7 +52,6 @@ namespace Strife.Blazor.Client
         private static async Task LoadAppSettingsAsync(WebAssemblyHostBuilder builder)
         {
             var client = new HttpClient() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
-            // the appsettings file must be in 'wwwroot'
             using var response = await client.GetAsync("appsettings.json");
             using var stream = await response.Content.ReadAsStreamAsync();
             builder.Configuration.AddJsonStream(stream);
